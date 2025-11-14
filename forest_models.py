d8983955f668ddeee7ee6dc15d154d363e0f3513
Rowan Williams <rowanwilliams263@gmail.com>
2025-11-14 16:30:30 -0700

    Added link to the paper for convenience

---
+++ forest_models.py
@@ -0,0 +1,14 @@
+# sklearn -- binary tree
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+# sklearn -- ensemble forests
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    RandomForestRegressor,
+    ExtraTreesClassifier,
+    ExtraTreesRegressor,
+)
+
+# Features are different amounts of time in the past
+ 
+# Is tomorrow up from today?
